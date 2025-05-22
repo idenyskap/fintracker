4e83d6238aaf1354578d4b6252106457869d3f2b
idenyskap <business.kapralov@gmail.com>
2025-05-22 18:34:03 +0300

    feat: new entities

---
+++ src/main/java/com/fintracker/entity/Category.java
@@ -0,0 +1,11 @@
+package com.fintracker.entity;
+
+import lombok.Data;
+
+@Data
+public class Category {
+
+  private String food;
+  private String transport;
+  private String housing;
+}
---
+++ src/main/java/com/fintracker/entity/Transaction.java
@@ -0,0 +1,7 @@
+package com.fintracker.entity;
+
+public class Transaction {
+
+  private String expenses;
+  private String income;
+}
